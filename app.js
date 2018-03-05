0508d735e940c76945102489f925d099f17e7400
siameseguy <psyhakhom@gmail.com>
2018-03-05 18:00:16 -0500

    working on special uri

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express() // creates an instance of an express application
 
+app.use('/news', (req, res, next) => {
+  console.log('Request Type:', req.method)
+  console.log('Path:', req.path)
+  res.status(200).send('This is the news page!')
+  next()
+})
+
+app.use('/*/special/', (req, res, next) => {
+  res.status(200).send('This is the special page!')
+  next()
+})
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(3000, () => {
