0d5d9b5ae4628961343a454baa3e86687d679479
Jared E Jackson <jaredjxn15@gmail.com>
2018-03-06 14:30:38 -0500

    made changes

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,17 @@ const tweetBank = require('../tweetBank');
 
 router.get('/', function(req, res) {
     let tweets = tweetBank.list();
-    res.render( 'index', {tweets:tweets});
+    res.render( 'index', {tweets: tweets});
 });
 
 
+router.get('/users/:name', function(req, res) {
+    var name = req.params.name;
+    var tweets = tweetBank.find( {name: name});
+    console.log(name, "name")
+    console.log(tweets)
+    res.render( 'index', { tweets: tweets } );
+  });
 
 
 module.exports = router;
