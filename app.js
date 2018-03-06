e4a872da3cff7840a98083cc2d3e7cc4f14dc7eb
Jared E Jackson <jaredjxn15@gmail.com>
2018-03-06 10:26:44 -0500

    started nunjucks

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express() // creates an instance of an express application
+const morgan = require('morgan');
+const nunjucks = require('nunjucks');
 
 app.use('/news', (req, res, next) => {
   console.log('Request Type:', req.method)
@@ -8,13 +10,33 @@ app.use('/news', (req, res, next) => {
   next()
 })
 
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+
+var locals = {
+  title: 'An Example',
+  people: [
+      { name: 'Gandalf'},
+      { name: 'Frodo' },
+      { name: 'Hermione'}
+  ]
+};
+
 app.use('/*/special/', (req, res, next) => {
   res.status(200).send('This is the special page!')
   next()
 })
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(morgan('combined'));
 
 app.listen(3000, () => {
   console.log('server listening on port 3000')
 })
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+
+nunjucks.configure('views');
+nunjucks.render('index.html', locals, function (err, output) {
+  console.log(output);
+});
+app.get('/', (req, res) => res.render('index.html'));
